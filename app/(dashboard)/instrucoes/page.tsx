c8de20ceae895c7f224863c43badab74c5c0ba4a
fernandodamico <fernando.damico@damicoed.com.br>
2026-04-22 09:08:00 -0300

    feat: add instrucoes de uso page

---
+++ app/(dashboard)/instrucoes/page.tsx
@@ -0,0 +1,7 @@
+export default function InstrucoesPage() {
+  return (
+    <div dangerouslySetInnerHTML={{ __html: html }} />
+  )
+}
+
+const html = `[cole aqui o conteúdo do body do HTML do documento 3]`
